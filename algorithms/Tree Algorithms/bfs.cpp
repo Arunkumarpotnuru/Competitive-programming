b75f14186ab8b4427795b04320f415bab9a39422
Arun kumar potnuru <87481875+Arunkumarpotnuru@users.noreply.github.com>
2024-06-15 10:16:21 +0530

    Update bfs.cpp

--- algorithms/Tree Algorithms/bfs.cpp
+++ algorithms/Tree Algorithms/bfs.cpp
@@ -1,1 +1,27 @@
+// bfs or level order traversal
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
 
+        while(!q.empty())
+        {
+            vector<int>level;
+            int sz = q.size();
+            for(int i=0;i<sz;i++)
+            {
+                TreeNode* curr= q.front(); q.pop();
+                level.push_back(curr->val);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            ans.push_back(level);
+            
+        }
+
+        return ans;
+    }
+};
